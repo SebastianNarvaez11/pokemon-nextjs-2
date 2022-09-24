1a57cfdc62c1de86f79fc368a603641078c3937e
Sebastian Narvaez <narvaez.jhoan@correounivalle.edu.co>
2022-09-24 16:25:49 -0500

    fix colors

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -40,19 +40,19 @@ export const Layout: FC<Props> = ({ title, children }) => {
                     <Sidebar />
                 </Grid>
 
-                <Grid xs={12} sm={12} md={11} lg={11} xl={11} css={{ flexDirection: 'column' }}>
+                <Grid xs={12} sm={8} md={7} lg={7} xl={7} css={{ flexDirection: 'column' }}>
                     <Navbar />
                     <main style={{ padding: 20 }}>
                         {children}
                     </main>
                 </Grid>
 
-                {/* <Grid xs={12} sm={4} md={4} lg={4} xl={4} css={{ padding: 20, boxShadow: '-1px -2px 5px 0px #0000001f', backgroundColor: getColorByType(pokemonSelected.full?.types[0].type.name) }}>
+                <Grid xs={12} sm={4} md={4} lg={4} xl={4} css={{ padding: 20, boxShadow: '-1px -2px 5px 0px #0000001f', backgroundColor: getColorByType(pokemonSelected.full?.types[0].type.name) }}>
                     {pokemonSelected.id !== 0 ?
                         <Image src={pokemonSelected.img} width={'500px'} height={'500px'} />
                         :
                         <Text>Seleccione un pokemon</Text>}
-                </Grid> */}
+                </Grid>
 
             </Grid.Container>
         </NextUIProvider>
